d1c5680dd494c237f72f92679466e818b7e1e6b3
hongtay <xanmangnga872@gmail.com>
2026-03-27 14:10:03 +0700

    exercise:functions

--- ex-1.js
+++ ex-1.js
@@ -2,16 +2,20 @@
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 
 function addSalary5000(previousSalary) {
-  // Start coding here
+  return previousSalary + 5000
 }
 
 function forEach(array, operation) {
-  // Start coding here
+  let employee = []
+  for (let i = 0 ; i < array.length ; i++){
+    employee.push(operation(array[i]))
+  }
+  return employee
 }
 
 // Using `forEach` function here
 
-let newEmployeeSalaries;
+let newEmployeeSalaries = forEach(employeeSalaries,addSalary5000);
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
 
@@ -19,8 +23,8 @@ console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
 ====================================
 
 1. ใน Exercise นี้ ฟังก์ชันใดเป็น Callback Function?
-
+addSalary5000
 2. ใน Exercise นี้ ฟังก์ชันใดเป็น Higher Order Function?
-
+forEach
 ====================================
 */
--- ex-2.js
+++ ex-2.js
@@ -5,16 +5,29 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 function checkGradeOver70(score) {
   // Start coding here
+  if (score > 70 ){
+    return true
+  }else if (score <= 70 ){
+    return false
+  }
 }
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let count = 0
+  for (let score of array){
+    if (operation(score)){
+      count ++
+
+    }
+  }
+  return count >=5
 }
 
 // Using `atLeastFive` function here
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1,checkGradeOver70);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2,checkGradeOver70);;
+let scoreRoom3Result =atLeastFive(studentScoresRoom3,checkGradeOver70);
 
 console.log(scoreRoom1Result); //true
 console.log(scoreRoom2Result); //false
@@ -24,8 +37,9 @@ console.log(scoreRoom3Result); //false
 ====================================
 
 1. ใน Exercise นี้ ฟังก์ชันใดเป็น Callback Function?
+checkGradeOver70
 
 2. ใน Exercise นี้ ฟังก์ชันใดเป็น Higher Order Function?
-
+atLeastFive
 ====================================
 */
